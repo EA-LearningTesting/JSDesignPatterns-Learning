8221e5cc5e3d50172de0bb2a4ff7016e8f595cff
Eslem <j.eslem03@gmail.com>
2015-06-25 09:38:14 +0200

    knockout with bindings

---
+++ Knockout/catClicker/js/app.js
@@ -0,0 +1,35 @@
+var Cat = function() {
+  this.clickCount = ko.observable(0);
+  this.name = ko.observable('Tabby');
+  this.levels = ko.observable('baby');
+  this.imgSrc = ko.observable('img/4154543904_6e2428c421_z.jpg');
+  this.imgAttribution = ko.observable('asdasdas');
+
+  this.title = ko.computed(function() {
+    var title;
+    var clicks = this.clickCount();
+    if (clicks < 10) {
+      title = 'Newborn';
+    } else if (clicks < 50) {
+      title = 'Infant';
+    } else if (clicks < 100) {
+      title = 'Child';
+    } else if (clicks < 500) {
+      title = 'Adult';
+    } else {
+      title = 'Ninja';
+    }
+    return title;
+  }, this);
+
+  this.nicknames = ko.observableArray(['nick1', 'nick2', 'nick3']);
+};
+
+var ViewModel = function() {
+  this.currentCat = ko.observable(new Cat());
+  this.incrementCounter = function() {
+    this.clickCount(this.clickCount() + 1);
+  };
+};
+
+ko.applyBindings(new ViewModel());
