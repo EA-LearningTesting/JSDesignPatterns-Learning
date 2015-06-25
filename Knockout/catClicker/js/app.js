d1c21b1b0f25148695eaeb5c82093e2839f78d62
Eslem <j.eslem03@gmail.com>
2015-06-25 10:01:26 +0200

    knockout array binding

--- Knockout/catClicker/js/app.js
+++ Knockout/catClicker/js/app.js
@@ -1,9 +1,43 @@
-var Cat = function() {
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('Tabby');
-  this.levels = ko.observable('baby');
-  this.imgSrc = ko.observable('img/4154543904_6e2428c421_z.jpg');
-  this.imgAttribution = ko.observable('asdasdas');
+var initialCats = [{
+    clickCount: 0,
+    name: 'Tabby',
+    imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+    imgAttribution: 'attr',
+    nicknames: ['nick1', 'nick2', 'nick3']
+  }, {
+    clickCount: 0,
+    name: 'Tiger',
+    imgSrc: 'img/22252709_010df3379e_z.jpg',
+    imgAttribution: 'attr',
+    nicknames: ['nick1', 'nick2', 'nick3']
+  }, {
+    clickCount: 0,
+    name: 'Tabby',
+    imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+    imgAttribution: 'attr',
+    nicknames: ['nick1', 'nick2', 'nick3']
+  }, {
+    clickCount: 0,
+    name: 'Scaredy',
+    imgSrc: 'img/1413379559_412a540d29_z.jpg',
+    imgAttribution: 'attr',
+    nicknames: ['nick1', 'nick2', 'nick3']
+  }, {
+    clickCount: 0,
+    name: 'Shadow',
+    imgSrc: 'img/9648464288_2516b35537_z.jpg',
+    imgAttribution: 'attr',
+    nicknames: ['nick1', 'nick2', 'nick3']
+  }
+
+];
+
+
+var Cat = function(data) {
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.imgAttribution = ko.observable(data.imgAttribution);
 
   this.title = ko.computed(function() {
     var title;
@@ -22,13 +56,26 @@ var Cat = function() {
     return title;
   }, this);
 
-  this.nicknames = ko.observableArray(['nick1', 'nick2', 'nick3']);
+  this.nicknames = ko.observableArray(data.nicknames);
 };
 
 var ViewModel = function() {
-  this.currentCat = ko.observable(new Cat());
+  var self = this;
+
+  this.catList = ko.observableArray([]);
+
+  initialCats.forEach(function(catItem) {
+    self.catList.push(new Cat(catItem));
+  });
+
+  this.currentCat = ko.observable(this.catList()[0]);
+
   this.incrementCounter = function() {
-    this.clickCount(this.clickCount() + 1);
+    self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+  };
+
+  this.setCurrentCat = function(item) {
+    self.currentCat(item);
   };
 };
 
